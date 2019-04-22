fa71e95aa9ca32ff6e143dcd59c8fb48ffb907d9
worldfilm <guo565656@gmail.com>
2019-04-22 11:41:31 +0800

    no message

--- src/router.js
+++ src/router.js
@@ -1,14 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/page/home.vue'
+import Home from '@/page/Home.vue'
+import Vip from '@/page/Vip.vue'
 Vue.use(Router)
 const router = new Router({
   mode: 'history',
   linkActiveClass: 'active',
   routes: [{
-      path: '/',
+      path: '/Home',
       name: 'Home',
       component: Home,
-    }]
+    },{
+        path: '/',
+        name: 'Vip',
+        component: Vip,
+      },]
 })
 export default router
